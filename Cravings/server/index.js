93a994244dfcbc927ab2f4e564962a9860641a89
Guptaryan007 <aryangupta291002@gmail.com>
2026-01-29 19:07:00 +0530

    cravings

--- Cravings/server/index.js
+++ Cravings/server/index.js
@@ -1,26 +1,22 @@
-import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import cloudinary from "./src/config/cloudinary.js";
 import cookieParser from "cookie-parser";
 import connectDB from "./src/config/db.js";
 import AuthRouter from "./src/routers/authRouter.js";
-import publicRouter from "./src/routers/publicRouter.js"
-import UserRouter from "./src/routers/userRouter.js"
+import publicRouter from "./src/routers/publicRouter.js";
+import UserRouter from "./src/routers/userRouter.js";
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173",credentials:true
- 
- }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
 
 app.use("/auth", AuthRouter);
 app.use("/public", publicRouter);
-app.use("/user", UserRouter)
-
+app.use("/user", UserRouter);
 
 app.get("/", (req, res) => {
   console.log("Server is Working");
@@ -34,7 +30,13 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log("Server Started at Port: ", port);
   connectDB();
+  try {
+    const res = await cloudinary.api.ping();
+    console.log("Cloudinary API is Working:", res);
+  } catch (error) {
+    console.error("Error Connecting Cloudinary:".error);
+  }
 });
--- Cravings/server/src/controllers/authController.js
+++ Cravings/server/src/controllers/authController.js
@@ -1,16 +1,23 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcrypt";
 import { genToken } from "../utils/authToken.js";
+
 export const UserRegister = async (req, res, next) => {
   try {
-    //accept data from frontend
-    const { fullName, email, mobileNumber, password } = req.body;
-    if (!fullName || !email || !mobileNumber || !password) {
+    console.log(req.body);
+    //accept data from Frontend
+    const { fullName, email, mobileNumber, password, role } = req.body;
+
+    //verify that all data exist
+    if (!fullName || !email || !mobileNumber || !password || !role) {
       const error = new Error("All feilds required");
       error.statusCode = 400;
       return next(error);
     }
 
+    console.log({ fullName, email, mobileNumber, password });
+
+    //Check for duplaicate user before registration
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       const error = new Error("Email already registered");
@@ -18,39 +25,51 @@ export const UserRegister = async (req, res, next) => {
       return next(error);
     }
 
+    console.log("Sending Data to DB");
+
     //encrypt the password
     const salt = await bcrypt.genSalt(10);
-
     const hashPassword = await bcrypt.hash(password, salt);
+
+    console.log("Password Hashing Done. hashPassword = ", hashPassword);
+
+    const photoURL = `https://placehold.co/600x400?text=${fullName.charAt(0).toUpperCase()}`;
+    const photo = {
+      url: photoURL,
+    };
 
     //save data to database
     const newUser = await User.create({
       fullName,
-      email,
+      email: email.toLowerCase(),
       mobileNumber,
       password: hashPassword,
+      role,
+      photo,
     });
 
-    // send response to frontend
+    // send response to Frontend
     console.log(newUser);
     res.status(201).json({ message: "Registration Successfull" });
-
     //End
   } catch (error) {
     next(error);
   }
 };
+
 export const UserLogin = async (req, res, next) => {
   try {
-    //fetch data from frontend
+    //Fetch Data from Frontend
     const { email, password } = req.body;
 
+    //verify that all data exist
     if (!email || !password) {
       const error = new Error("All feilds required");
       error.statusCode = 400;
       return next(error);
     }
 
+    //Check if user is registred or not
     const existingUser = await User.findOne({ email });
     if (!existingUser) {
       const error = new Error("Email not registered");
@@ -58,8 +77,7 @@ export const UserLogin = async (req, res, next) => {
       return next(error);
     }
 
-    // verify the password
-
+    //verify the Password
     const isVerified = await bcrypt.compare(password, existingUser.password);
     if (!isVerified) {
       const error = new Error("Password didn't match");
@@ -68,15 +86,16 @@ export const UserLogin = async (req, res, next) => {
     }
 
     //Token Generation will be done here
-    await genToken(existingUser, res);
+    genToken(existingUser, res);
 
     //send message to Frontend
     res.status(200).json({ message: "Login Successfull", data: existingUser });
-    //end
+    //End
   } catch (error) {
     next(error);
   }
 };
+
 export const UserLogout = async (req, res, next) => {
   try {
     res.clearCookie("parleG");
